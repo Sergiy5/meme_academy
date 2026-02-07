c6113de2492fc2d90967f8e6dadbed679188a92c
Serhii Kushnir <s1983kushnir@gmail.com>
2026-02-07 19:13:31 +0100

    Added  8 avatars

---
+++ public/images/avatars/index.ts
@@ -0,0 +1,10 @@
+import ducky from './ducky.webp';
+import fokario from './fokario.webp';
+import foxy from './foxy.webp';
+import kitty from './kitty.webp';
+import owly from './owly.webp';
+import pingui from './pingui.webp';
+import racky from './racky.webp';
+import wolfy from './wolfy.webp';
+
+export { ducky, fokario, foxy, kitty, owly, pingui, racky, wolfy };
---
+++ src/data/avatarData.ts
@@ -0,0 +1,58 @@
+import{
+  ducky, fokario, foxy, kitty, owly, pingui, racky, wolfy
+} from '../../public/images/avatars/index';
+
+export const bgAvatarColors = [
+  '#FF383C',
+  '#FF8D28',
+  '#FFCC00',
+  '#34C759',
+  '#00C8B3',
+  '#00C0E8',
+  '#0088FF',
+  '#9555F5',
+  '#CB30E0',
+];
+
+export const avatars = [
+  {
+    id: 1,
+    name: 'Ducky',
+    imgUrl: ducky,
+  },
+  {
+    id: 2,
+    name: 'Fokario',
+    imgUrl: fokario,
+  },
+  {
+    id: 3,
+    name: 'Foxy',
+    imgUrl: foxy,
+  },
+  {
+    id: 4,
+    name: 'Kitty',
+    imgUrl: kitty,
+  },
+  {
+    id: 5,
+    name: 'Owly',
+    imgUrl: owly,
+  },
+  {
+    id: 6,
+    name: 'Pingui',
+    imgUrl: pingui,
+  },
+  {
+    id: 7,
+    name: 'Racky',
+    imgUrl: racky,
+  },
+  {
+    id: 8,
+    name: 'Wolfy',
+    imgUrl: wolfy,
+  },
+];
